f6215a3540fff69cc99aad0a06b032bbe77f516c
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-08-28 21:52:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ with st.expander('Analizar texto'):
         translation = translator.translate(text, src="es", dest="en")
         trans_text = translation.text
         blob = TextBlob(trans_text)
-        blob2="this is a phrse"
+        blob2=TextBlob("this is a phrse")
         st.write((blob2.correct())) 
         st.write('Polarity: ', round(blob.sentiment.polarity,2))
         st.write('Subjectivity: ', round(blob.sentiment.subjectivity,2))
