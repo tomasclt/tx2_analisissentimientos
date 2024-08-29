f7376d66d9cefa169bf8cceffc2dc7200c7df546
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-08-28 22:24:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from textblob import TextBlob
 import pandas as pd
 import streamlit as st
-#import nltk_download.py
+import nltk_download.py
 from PIL import Image
 from googletrans import Translator
 
